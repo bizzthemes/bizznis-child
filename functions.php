32dfc7b949ee0f574bb8d7cfddbd53dc68f72c8d
bizzthemes <info@bizzthemes.com>
2014-01-24 16:15:20 +0100

    Update styling

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ load_template( get_template_directory() . '/lib/init.php' );
 //* Child theme (do not remove)
 define( 'CHILD_THEME_NAME', __( 'Bizznis Child Theme', 'bizznis-child' ) );
 define( 'CHILD_THEME_URL', 'https://github.com/bizzthemes/bizznis-child/' );
-define( 'CHILD_THEME_VERSION', '1.0' );
+define( 'CHILD_THEME_VERSION', '1.0.5' );
 
 //* Set Localization (do not remove)
 load_child_theme_textdomain( 'bizznis-child', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'bizznis-child' ) );
